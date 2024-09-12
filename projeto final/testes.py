6d2dd72688b6b7cb35d05256384f04aa6c39ea67
marisarinho <164221246+marisarinho@users.noreply.github.com>
2024-09-12 15:00:19 -0300

    Add files via upload

---
+++ projeto final/testes.py
@@ -0,0 +1,24 @@
+from func_projeto import *
+
+x = input("Digite o nome da cidade: ").upper()
+localizar_cod(x)
+
+# #Lista com o nome das cidades do arquivo fonte:
+# arquivo = open("Nome_cidades.txt","r")
+# NM_UE = arquivo.readlines()
+# arquivo.close()
+
+# #Lista com o codigo das cidades do arquivo fonte:
+# arquivo = open("Codigo_UE.txt","r")
+# SE_UE = arquivo.readlines()
+# arquivo.close()
+
+# print(NM_UE)
+# nome_cid = remover_repet(montar_lista(NM_UE))
+# cod_cid = remover_repet(montar_lista(SE_UE))
+
+# if x in nome_cid:
+#     i = nome_cid.index(x)
+#     print(f"Codigo da cidade {x} é: {cod_cid[i]}")
+# else:
+#     print("cidade não encontrada!")
